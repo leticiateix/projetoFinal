a7aea921d6a2dba6ff684b46b1f128faa126d915
Victoria Beatriz <victoria.beatriz@ufba.br>
2022-11-20 14:46:51 -0300

    Iniciando o rascunho da página inicial

--- INFOMART/src/App.jsx
+++ INFOMART/src/App.jsx
@@ -7,26 +7,48 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      {/*<div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  </div>*/}
+      <nav>
+        <div className="navbarcontato">
+          <h4>(XX) YYYYY - ZZZZ</h4>
+          <h4>contato@infomart.com</h4>
+        </div>
+        <div className="navbar">
+          <div className="logo">
+            <a href="#">InfoLists</a>
+          </div>
+          <div className="items">
+            <a href="#">Meus Favoritos</a>
+            <a href="#">Categorias</a>    
+          </div>
+          <input type="text" name="pesquisa" placeholder="Placeholder"></input>
+          <div className="cart">
+            <a href="#">Minha Lista</a>
+          </div>
+        </div>
+      </nav>
+      <body>
+        <div className="hero">
+          <h1>Listas de compra nunca foram tão simples!</h1>
+          <p>Com um clique construa sua lista de compras gratuitamente de forma rápida</p>
+        </div>
+        <div className="destaques">
+          <h3>Algumas sugestões</h3>
+          <h3>CARROSSEL</h3>
+        </div>
+        <div className="promocoes">
+          <h2>Categorias</h2>
+          <div className="frame">
+            
+          </div>
+        </div>
+      </body>
+      
+      
     </div>
   )
 }
