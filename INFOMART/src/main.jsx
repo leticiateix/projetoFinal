1be905d79d884f63af6999ef69a9439f41c34009
Victoria Beatriz <victoria.beatriz@ufba.br>
2022-11-30 20:43:20 -0300

    Adicionando Rota home

--- INFOMART/src/main.jsx
+++ INFOMART/src/main.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from 'react-router-dom';
 import './index.css'
+import Home from "./pages/home";
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
 
